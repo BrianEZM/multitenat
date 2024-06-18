31cf16b97cae2c0afba6b2883b7ba90d3a73ae7c
Brian Emanuel Zapata Marin <brianezm@gmail.com>
2024-06-18 12:37:10 -0300

    implementando AOP + AbstractDataSource

---
+++ src/main/kotlin/com/_ddbb/brianezm/domain/infra/config/MultitenantConfiguration.kt
@@ -0,0 +1,42 @@
+package com._ddbb.brianezm.domain.infra.config
+
+import org.hibernate.cfg.Environment
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource
+
+@Configuration
+class MultitenantConfiguration(
+    @Value("\${spring.tenant.default}") private val defaultTenant: String,
+    private val environment: Environment
+) {
+
+    @Bean
+    fun dataSource(): AbstractRoutingDataSource {
+        val resolvedDataSources = mutableMapOf<Any, Any>()
+
+        // Configuración tenant "master"
+        val masterDataSourceBuilder = DataSourceBuilder.create()
+            .driverClassName(environment.getProperty("spring.master.datasource.driver-class-name")) // Adaptado a tu estructura
+            .username(environment.getProperty("spring.master.datasource.username"))
+            .password(environment.getProperty("spring.master.datasource.password"))
+            .url(environment.getProperty("spring.master.datasource.jdbc-url"))
+        resolvedDataSources["master"] = masterDataSourceBuilder.build()
+
+        // Configuración tenant "read-only"
+        val readOnlyDataSourceBuilder = DataSourceBuilder.create()
+            .driverClassName(environment.getProperty("spring.read-only.datasource.driver-class-name")) // Adaptado a tu estructura
+            .username(environment.getProperty("spring.read-only.datasource.username"))
+            .password(environment.getProperty("spring.read-only.datasource.password"))
+            .url(environment.getProperty("spring.read-only.datasource.jdbc-url"))
+        resolvedDataSources["read-only"] = readOnlyDataSourceBuilder.build()
+
+        val dataSource = MultitenantDataSource()
+        dataSource.setTargetDataSources(resolvedDataSources)
+        resolvedDataSources[defaultTenant]?.let { dataSource.setDefaultTargetDataSource(it) }
+        dataSource.afterPropertiesSet()
+        return dataSource
+    }
+}
--- src/main/kotlin/com/_ddbb/brianezm/domain/infra/config/MultitenantDataSource.kt
+++ src/main/kotlin/com/_ddbb/brianezm/domain/infra/config/MultitenantDataSource.kt
@@ -1,29 +1,10 @@
 package com._ddbb.brianezm.domain.infra.config
 
-import jakarta.annotation.PostConstruct
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource
-import org.springframework.stereotype.Component
-import javax.sql.DataSource
 
-@Component
-class MultitenantDataSource(
-    @Qualifier("masterDataSource") private val masterDataSource: DataSource,
-    @Qualifier("readOnlyDataSource") private val readOnlyDataSource: DataSource
-) : AbstractRoutingDataSource() {
+class MultitenantDataSource : AbstractRoutingDataSource() {
 
     override fun determineCurrentLookupKey(): Any {
-        val tenantId = TenantContext.getCurrentTenant() ?: "master"
-        println("Tenant resuelto en MultitenantDataSource: $tenantId") // Agrega este log
-        return tenantId
-    }
-
-    @PostConstruct
-    override fun initialize() {
-        val targetDataSources = mutableMapOf<Any, Any>()
-        targetDataSources["master"] = masterDataSource
-        targetDataSources["read-only"] = readOnlyDataSource // Asegúrate de que el nombre coincida con el tenant
-        setTargetDataSources(targetDataSources)
-        setDefaultTargetDataSource(masterDataSource)
+        return TenantContext.getCurrentTenant() ?: "master" // Usamos el TenantContext para obtener el tenant
     }
 }
